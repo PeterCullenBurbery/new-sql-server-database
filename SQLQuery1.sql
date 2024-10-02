4645ff9b803ad6e537550de77a2c51a626a8b4f9
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-10-02 15:44:02 -0400

    I added more details to the SQL file.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,16 @@
 create database mynewdatabase;
 go
 use mynewdatabase;
+
+CREATE TABLE mynewtable (
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(100),
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+GO
+
+INSERT INTO mynewtable (ID, Name) 
+VALUES (2, 'Sample Data');
+GO
+
+select * from mynewtable;
